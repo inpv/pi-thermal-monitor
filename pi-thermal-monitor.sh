565b041b149882645bbb384618a6b5bd7ce88817
inpv <inpv@protonmail.ch>
2025-07-03 00:29:55 +0500

    Optimize GPU measure call for latest Bookworm OS

--- pi-thermal-monitor.sh
+++ pi-thermal-monitor.sh
@@ -9,7 +9,7 @@ do
   echo -e "\e[95m:::::::::::\e[96mTHERMAL MONITOR\\e[95m:::::::::::"
   echo ""
   echo -e "\e[0m\e[94mCPU \e[0mtemp=$cpu_temp1.$cpu_temp_display'C"
-  echo -e "\e[32mGPU \e[0m$(/opt/vc/bin/vcgencmd measure_temp)"
+  echo -e "\e[32mGPU \e[0m$(vcgencmd measure_temp)"
   echo ""
   echo -e "\e[91mCtrl\e[32m+\e[91mC\e[0m to exit..."
   echo -e "\e[95m:::::::::::::::::::::::::::::::::::::"
